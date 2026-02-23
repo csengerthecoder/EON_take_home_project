84200e98ee0c142d01aab9a91595392034fbb5c0
csengerthecoder <191019948+csengerthecoder@users.noreply.github.com>
2026-02-23 14:58:18 +0100

    sign up tests done, still working on CI

--- src/test/java/project/tests/smoke/SignUpTests.java
+++ src/test/java/project/tests/smoke/SignUpTests.java
@@ -3,6 +3,8 @@ package project.tests.smoke;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import project.baseTest.BaseTest;
 import project.pages.RegisterPage;
 
@@ -26,4 +28,25 @@ public class SignUpTests extends BaseTest {
 
         Assertions.assertFalse(homePage.isSettingsButtonDisplayed());
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "test1, '', asd123",
+            "'', test22@gmail.com, asd123",
+            "test33, test33@gmail.com, ''",
+            "test33, '', ''",
+            "'', test33@gmail.com, ''",
+            "'', '', asd123",
+            "'', '', ''",
+    })
+    @Tag("known-issue")
+    void testMissingCredentialSignUps(String username, String email, String password) {
+        RegisterPage registerPage = homePage.clickRegister();
+        registerPage.fillOutSignUpForm(username, email, password);
+        registerPage.clickSignUpButton();
+
+        Assertions.assertFalse(homePage.isSettingsButtonDisplayed());
+    }
+
+
 }
